77083f8199917af0aec240c7f8ca64b3f40c92c8
Oj Sindher <sindher.oj@gmail.com>
2020-05-27 15:10:22 +0530

    Completed solution
    Function to display the dataset

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -18,6 +18,9 @@ figure; % open a new figure window
 
 
 
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data 
+ylabel('Profit in $10,000s'); % Set the y−axis label 
+xlabel('Population of City in 10,000s'); 
 
 
 % ============================================================
