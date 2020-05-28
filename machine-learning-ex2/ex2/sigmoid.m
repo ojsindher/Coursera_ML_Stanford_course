6b6e91d6ee293a1352674faa87f465b222456d80
Oj Sindher <sindher.oj@gmail.com>
2020-05-28 22:33:46 +0530

    Completed sigmoid function

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -9,9 +9,7 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
-
+g = (ones(size(z)))./(ones(size(z))+(exp(-z)));
 
 % =============================================================
 
