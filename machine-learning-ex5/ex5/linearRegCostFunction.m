fdbfb74749d8adc974c321924dc8960d3638795e
Oj Sindher <sindher.oj@gmail.com>
2020-05-31 15:15:05 +0530

    Completed function to calculate LinReg cost's function
    Regularized linear regression cost function

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,11 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = (1/(2*m))*sum(((X*theta)-y).^2) + ( (lambda/(2*m))*(theta(2:end,:)'*theta(2:end,:)) ) ;
 
-
-
-
-
-
-
-
-
-
+h = X*theta;
+theta(1) = 0;
+grad = (1/m)*(X'*(h - y)) + (lambda/m)*(theta) ;
 
 % =========================================================================
 
